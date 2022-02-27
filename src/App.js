1983e748bf63a4ff3b67b2412945c36ce6593b2b
Abhay1217 <80531163+Abhay1217@users.noreply.github.com>
2022-02-27 21:57:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import TicTacToe from "./tictactoe/Tictactoe";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        {/* <h1>This is TicTacToe Game</h1> */}
+        <TicTacToe />
+      </div>
+    </div>
+  );
+}
+
+export default App;
